9ca5e6014e63c5798fb6ae8c068d1b44d162323d
LiHang Yang <863141163@qq.com>
2021-05-04 17:28:43 +0800

    ADD: zigzag convert

---
+++ internal/convert_z.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"fmt"
+	"strings"
+)
+
+/**
+https://leetcode-cn.com/problems/zigzag-conversion/
+*/
+func convertZ(s string, numRows int) string {
+	if numRows < 2 {
+		return s
+	}
+
+	res := make([]string, numRows)
+	for i := range res {
+		res[i] = ""
+	}
+
+	flag := -1
+	i := 0
+
+	for _, r := range s {
+		res[i] = res[i] + fmt.Sprintf("%c", r)
+		if i == 0 || i == numRows-1 {
+			flag = -flag
+		}
+		i += flag
+	}
+
+	return strings.Join(res, "")
+}
